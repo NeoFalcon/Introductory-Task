8613b39f473ae8057eff8d98f4075027fd7571fc
Jose Rojas <jose.rojas@laundrylan.com.au>
2018-07-23 20:43:24 +1000

    Facebook Login / Register

---
+++ IntroductoryTask/Data/IntroductoryTaskContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IntroductoryTask.Data
+{
+	public class IntroductoryTaskContext : DbContext
+	{
+		public IntroductoryTaskContext(DbContextOptions<IntroductoryTaskContext> options)
+			: base(options)
+		{
+			Database.Migrate();
+		}
+
+		public DbSet<Payment> tblPayments { get; set; }
+	}
+}
