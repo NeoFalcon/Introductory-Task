27fe28e9f9a7c7e7e6b5b4bb0c36d98b151b54d7
Jose Rojas <jose.rojas@laundrylan.com.au>
2018-07-24 22:52:22 +1000

    Facebook Login / Registration completed

--- IntroductoryTask/ClientApp/components/Home.tsx
+++ IntroductoryTask/ClientApp/components/Home.tsx
@@ -4,8 +4,6 @@ import { RouteComponentProps } from 'react-router-dom';
 export default class Home extends React.Component<RouteComponentProps<{}>, {}> {
     public render() {
         return <div>
-			<h1>Registration / Login</h1>
-
 			<div
 				className='fb-login-button'
 				data-max-rows='1'
